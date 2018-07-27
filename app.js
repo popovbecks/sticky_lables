f2e565e2e244c0100b76001dc65e65c2c09bfbbb
Oleksii Popov <oleksiipopov@admins-mbp.synapse.com>
2018-07-27 18:32:22 +0300

    Refactored code firstElement disapear after scrolling

--- app.js
+++ app.js
@@ -10,19 +10,23 @@ list.onscroll = function() {
 function getCoords() {
      for(let i = 0; i < labels.length; i++) {
          if (list.scrollTop > (labels[i].offsetTop - list.offsetTop) - 23) {
-             labelFirst.classList.add('hide-first');
-             labels[i].classList.add('hide');
+
+             labelFirst.classList.add('hide');
+             //labels[i].classList.add('hide');
          }
 
          if(list.scrollTop > (labels[i].offsetTop - list.offsetTop)){
-             labelFirst.classList.remove('hide-first');
-             labelFirst.innerHTML = labels[i].innerHTML;
+
              labels[i].classList.add('hide');
+             labelFirst.innerHTML = labels[i].innerHTML;
+             labelFirst.classList.remove('hide');
+
          } else{
-             labels[i].classList.remove('hide')
+             labels[i].classList.remove('hide');
          }
-         if(list.scrollTop < (labels[0].offsetTop - list.offsetTop)-23) {
+         if(list.scrollTop < (labels[0].offsetTop - list.offsetTop)-22) {
              labelFirst.innerHTML = temp;
+             labelFirst.classList.remove('hide');
          }
 
      }
